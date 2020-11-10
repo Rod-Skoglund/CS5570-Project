5ea7820f042e432f2ff09254c04b35b100537f35
erginbostanci@yahoo.com <erginbostanci@yahoo.com>
2020-11-09 19:59:17 -0600

    First Commit

--- Main.py
+++ Main.py
@@ -28,6 +28,22 @@ def main():
     transaction_count = 0
     type_count = 0
 
+    while(True):
+      transaction_count = input("Enter the number of transactions as an positive integer: " )
+      if transaction_count.isnumeric():
+          transaction_count=int(transaction_count)
+          if transaction_count>0:
+              break        
+      print("Please enter a numeric value greater than zero")
+
+    while(True):
+      type_count = input("Enter the number of data types as an positive integer less than 27: " )
+      if type_count.isnumeric():
+          type_count=int(type_count)
+          if type_count>0 and type_count<27:
+              break        
+      print("Please enter a numeric value greater than zero and less than 27")
+
     # Use input to randomly generate a history to be used in a scheduler
     tranaction_manager.generate_transactions(transaction_count, type_count)
     history = tranaction_manager.generate_history()    
